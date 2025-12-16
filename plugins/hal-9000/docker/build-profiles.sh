1e613c5fb768972650bc8ec9e3cad859ebe461d1
Hellblazer <hellblazer@me.com>
2025-12-15 18:49:10 -0800

    chore: Update Docker build script to version 1.3.0

--- plugins/hal-9000/docker/build-profiles.sh
+++ plugins/hal-9000/docker/build-profiles.sh
@@ -10,7 +10,7 @@ set -Eeuo pipefail
 
 readonly SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
 readonly IMAGE_BASE="ghcr.io/hellblazer/hal-9000"
-readonly VERSION="1.2.0"
+readonly VERSION="1.3.0"
 
 # Colors
 readonly GREEN='\033[0;32m'
